6b6ac9c0a6181dc454065d4cca7dec0672d3f192
Denis Golub <denys.golub@santander.co.uk>
2022-01-10 21:52:12 +0000

    day4 with tests

--- src/main/java/uk/co/adventOfCode/AdventOfCode/service/Day4.java
+++ src/main/java/uk/co/adventOfCode/AdventOfCode/service/Day4.java
@@ -1,39 +1,73 @@
 package uk.co.adventOfCode.AdventOfCode.service;
 
-import org.yaml.snakeyaml.util.ArrayUtils;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 public class Day4 {
 
     private static List<String> choices = new ArrayList<>();
-    private static List<List<String>> gameBoard = new ArrayList<>();
+    private static List<List<List<String>>> gameBoard = new ArrayList<>();
+    private static int winningNumber;
 
     public int bingo() {
+        int value1 = 0;
         getData();
-        
-        return 1;
+        int winningBoardNumber = getWinningBoard();
+        List<List<String>> winningBoard = gameBoard.get(winningBoardNumber);
+        for (int i = 0; i < winningBoard.size(); i++) {
+            value1 += Arrays.stream(winningBoard.get(i).stream().filter(x -> !x.equals("X")).mapToInt(x -> Integer.parseInt(x)).toArray()).sum();
+        }
+        return value1 * winningNumber;
 
     }
-    
-    private void getData() {
-        List<String> boardLine = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("/Users/e1206373/Desktop/AdventOfCode/src/test/resources/day4/testData"))) {
+    private int getWinningBoard() {
+        for (int i = 0; i < choices.size(); i++) {
+            for (int j = 0; j < gameBoard.size(); j++) {
+                List<List<String>> game = gameBoard.get(j);
+                for (int k = 0; k < game.size(); k++) {
+                    if (game.get(k).contains(choices.get(i))) {
+                        game.get(k).set(game.get(k).indexOf(choices.get(i)), "X");
+                        if (5 == game.get(k).stream().filter(x -> x.equals("X")).count() || getColoumnValues(gameBoard.get(j))) {
+                            winningNumber = Integer.parseInt(choices.get(i));
+                            return j;
+                        }
+                    }
+                }
+
+            }
+        }
+        System.out.println(gameBoard);
+        return 0;
+    }
+
+    private boolean getColoumnValues(List<List<String>> currentBoard) {
+        for (int i = 0; i < currentBoard.size(); i++) {
+            int xCount = 0;
+            for (int j = 0; j < currentBoard.get(0).size(); j++) {
+                if ("X" == currentBoard.get(j).get(i)) {
+                    xCount++;
+                    if (xCount == 5) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void getData() {
+        List<List<String>> boardLine = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/e1206373/Desktop/AdventOfCode/src/test/resources/day4/testData2"))) {
             choices.addAll(Arrays.asList(br.readLine().split(",")));
             while (br.readLine() != null) {
                 for (int i = 0; i < 5; i++) {
-                    boardLine.add(br.readLine());
+                    boardLine.add(Arrays.asList(br.readLine().trim().split("\\s+")));
                 }
                 gameBoard.add(boardLine);
                 boardLine = new ArrayList<>();
@@ -44,9 +78,5 @@ public class Day4 {
 
         System.out.println(choices);
         System.out.println(gameBoard);
-//        List<Integer> collect = gameBoard.get(0).stream().map(Integer::parseInt).collect(Collectors.toList());
-        Integer[] collect2 = Arrays.asList(gameBoard.get(0).get(0).split(" ")).stream().filter(x -> x == "").toArray(Integer[]::new);
-//        ArrayUtils.remove(array, index)
-//                .stream().map(s -> Integer.parseInt(s.trim())).collect(Collectors.toList());
     }
 }
